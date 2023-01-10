41a5ca157a90dccb263985c3c6ded9fcd30bec7a
Rictorre <113469188+Rictorre@users.noreply.github.com>
2023-01-09 20:01:10 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,75 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-const PORT = 1337;
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = 
+  `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map((post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span><a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+    
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        ).join("")}
+    </div>
+  </body>
+</html>`;
+  res.send(html);
+});
+app.get("/posts/:id", (req, res, next) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html = `<!DOCTYPE html> <html>
+  <head>
+  <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+<body>
+<div class="news-list">
+<header><img src="/logo.png"/>Wizard News</header>
+<div class='news-item'>
+    <p>
+      ${post.id} ${post.title}
+      (by ${post.name} ${post.content}
+        </p>
+      
+  </div> </body
+  ></htm
+  l>`;
+  if (!post.id) {
+    throw new Error("Not Found");
+  }
+
+  res.send(html);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Sorry, page not found");
+});
+
